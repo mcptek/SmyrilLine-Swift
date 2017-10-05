c765597f2f6075966bdd6afefd7a3d337162e261
Rafay Hasan <Rafay@Rafays-Mac-mini.local>
2017-10-05 16:21:03 +0600

    Changed taxfree title for details page

--- SmyrilLine/TaxfreeDetailsViewController.swift
+++ SmyrilLine/TaxfreeDetailsViewController.swift
@@ -24,7 +24,7 @@ class TaxfreeDetailsViewController: UIViewController, UITableViewDataSource, UIT
         
         self.navigationController?.navigationBar.isHidden = false
         self.navigationItem.backBarButtonItem = UIBarButtonItem.init(title: "Back", style: .plain, target: nil, action: nil)
-        self.title = "Tax free"
+        self.title = "Price and Menu"
         
         
         self.myHeaderView = Bundle.main.loadNibNamed("TaxfreeHeaderDetailsHeader", owner: self, options: nil)?.first as? UIView as! TaxfreeHeaderDetailsHeader
