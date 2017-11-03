45de2963109029769e901fc805396a47a12e46ed
Rafay Hasan <Rafay@Rafays-Mac-mini.local>
2017-11-03 10:04:19 +0600

    Before working on MealType details cell.

---
+++ SmyrilLine/MealTypeDescriptionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTypeDescriptionTableViewCell.swift
+//  SmyrilLine
+//
+//  Created by Rafay Hasan on 11/2/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+import UIKit
+
+class MealTypeDescriptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
